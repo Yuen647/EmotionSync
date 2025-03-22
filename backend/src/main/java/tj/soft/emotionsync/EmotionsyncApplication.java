6297cc477ffb2e6c04c28e85ef3208aebbabccd4
Sheerio <2900952121@qq.com>
2025-03-22 12:40:44 +0800

    initialize

---
+++ backend/src/main/java/tj/soft/emotionsync/EmotionsyncApplication.java
@@ -0,0 +1,13 @@
+package tj.soft.emotionsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionsyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tj/soft/emotionsync/EmotionsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package tj.soft.emotionsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
